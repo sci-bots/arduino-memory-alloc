40bb896f64521aca41162db4cc5d88cdf1ac6709
Christian Fobel <christian@fobel.net>
2017-08-01 00:12:14 -0400

    Add code and Conda build requirements

--- MemoryAlloc.cpp
+++
@@ -1,1 +0,0 @@
-#include "MemoryAlloc.h"
--- MemoryAlloc.h
+++ MemoryAlloc.h
@@ -1,10 +1,80 @@
-#ifndef ___MemoryAlloc__H___
-#define ___MemoryAlloc__H___
+#ifndef ___MEMORY_ALLOC__H___
+#define ___MEMORY_ALLOC__H___
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
 #include "WProgram.h"
 #endif
+#include <stdint.h>
+#include <LinkedList.h>
+#include <AlignedAlloc.h>
 
-#endif  // #ifndef ___MemoryAlloc__H___
+namespace memory_alloc {
+
+class MemoryAlloc {
+private:
+  LinkedList<uint32_t> allocations_;
+  LinkedList<uint32_t> aligned_allocations_;
+
+public:
+  uint32_t mem_alloc(uint32_t size) {
+    uint32_t address = (uint32_t)malloc(size);
+    // Save to list of allocations for memory management.
+    allocations_.add(address);
+    return address;
+  }
+  uint32_t mem_aligned_alloc(uint32_t alignment, uint32_t size) {
+    uint32_t address = (uint32_t)aligned_malloc(alignment, size);
+    // Save to list of allocations for memory management.
+    aligned_allocations_.add(address);
+    return address;
+  }
+  uint32_t mem_aligned_alloc_and_set(uint32_t alignment, UInt8Array data) {
+    // Allocate aligned memory.
+    const uint32_t address = mem_aligned_alloc(alignment, data.length);
+    if (!address) { return 0; }
+    // Copy data to allocated memory.
+    mem_cpy_host_to_device(address, data);
+    return address;
+  }
+  void mem_aligned_free(uint32_t address) {
+    for (int i = 0; i < aligned_allocations_.size(); i++) {
+      if (aligned_allocations_.get(i) == address) {
+        aligned_allocations_.remove(i);
+      }
+    }
+    aligned_free((void *)address);
+  }
+  void mem_cpy_host_to_device(uint32_t address, UInt8Array data) {
+    memcpy((uint8_t *)address, data.data, data.length);
+  }
+  void mem_fill_uint8(uint32_t address, uint8_t value, uint32_t size) {
+    mem_fill((uint8_t *)address, value, size);
+  }
+  void mem_fill_uint16(uint32_t address, uint16_t value, uint32_t size) {
+    mem_fill((uint16_t *)address, value, size);
+  }
+  void mem_fill_uint32(uint32_t address, uint32_t value, uint32_t size) {
+    mem_fill((uint32_t *)address, value, size);
+  }
+  void mem_fill_float(uint32_t address, float value, uint32_t size) {
+    mem_fill((float *)address, value, size);
+  }
+  void mem_free(uint32_t address) {
+    for (int i = 0; i < allocations_.size(); i++) {
+      if (allocations_.get(i) == address) { allocations_.remove(i); }
+    }
+    free((void *)address);
+  }
+  void mem_free_all() {
+    while (allocations_.size() > 0) { free((void *)allocations_.shift()); }
+    while (aligned_allocations_.size() > 0) {
+      aligned_free((void *)aligned_allocations_.shift());
+    }
+  }
+};
+
+}  // namespace memory_alloc {
+
+#endif  // #ifndef ___MEMORY_ALLOC__H___
